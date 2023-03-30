1af3cb765ff9fd5663c94122875aa3d3ae6bfda9
Joey <joeygeof.dev@gmail.com>
2023-03-30 19:23:29 -0400

    Update: server.py silly typo LOL

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -7,11 +7,11 @@ server = Flask(__name__)
 mysql = MySQL(server)
 
 # configuration for the application
-server.config["MYSQL_HOST"] = os.envision.get("MYSQL_HOST")
-server.config["MYSQL_USER"] = os.envision.get("MYSQL_USER")
-server.config["MYSQL_PASSWORD"] = os.envision.get("MYSQL_PASSWORD")
-server.config["MYSQL_DB"] = os.envision.get("MYSQL_DB")
-server.config["MYSQL_PORT"] = os.envision.get("MYSQL_PORT")
+server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
+server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
+server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
+server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
+server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
 
 # create route for login
 @server.route("/.login", methods=["POST"])
