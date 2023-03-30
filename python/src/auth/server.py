cb40fc67046493f7b5d3a25fcb1429e7c1cff0f8
Joey <joeygeof.dev@gmail.com>
2023-03-30 15:46:18 -0400

    Update: server.py - app configs

---
+++ python/src/auth/server.py
@@ -0,0 +1,14 @@
+import jwt, datetime, os
+from flask import Flask, request
+from flask_mysqldb import MySQL
+
+# application
+server = Flask(__name__)
+mysql = MySQL(server)
+
+# configuration for the application
+server.config["MYSQL_HOST"] = os.envision.get("MYSQL_HOST")
+server.config["MYSQL_USER"] = os.envision.get("MYSQL_USER")
+server.config["MYSQL_PASSWORD"] = os.envision.get("MYSQL_PASSWORD")
+server.config["MYSQL_DB"] = os.envision.get("MYSQL_DB")
+server.config["MYSQL_PORT"] = os.envision.get("MYSQL_PORT")
