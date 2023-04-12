877674a2d2792902a8b58abd0a51d959da99f140
Joey <joeygeof.dev@gmail.com>
2023-04-11 23:31:59 -0400

    Update: pika - connection to rabbitmq

--- python/src/gateway/server.py
+++ python/src/gateway/server.py
@@ -5,7 +5,16 @@ from auth import validate
 from auth_svc import access
 from storage import util
 
+# create application
 server = Flask(__name__)
+# set config option for MongoDB connection uri
 server.config['MONGO_URI'] = 'mongodb://host.minikube.internal:27017/videos'
 
+# provide an interface for working with MongoDB
 mongo = PyMongo(server)
+
+# allow storing and retrieving files using GridFS within MongoDB
+fs = gridfs.GridFS(mongo.db)
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+channel = connection.channel()
