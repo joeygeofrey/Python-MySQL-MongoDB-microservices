beed1e82b1808fe86827b92010ada33d3e79144c
Joey <joeygeof.dev@gmail.com>
2023-04-12 22:54:20 -0400

    Add: auth service

---
+++ python/src/gateway/auth_svc/access.py
@@ -0,0 +1,22 @@
+import os, requests
+
+# create login route to communicate with auth service
+def login(request):
+    auth = request.authorization
+    if not auth:
+        return None, ('missing credentials', 401)
+
+    # create basic auth tuple
+    basicAuth = (auth.username, auth.password)
+
+    # send http call to auth service
+    response = requests.post(
+        f"http://{os.environ.get('AUTH_SVC_ADDRESS')}/login",
+        auth=basicAuth
+    )
+
+    # check response
+    if response.status_code = 200:
+        return response.txt, None
+    else:
+        return None, (response.text, response.status_code) 
